7602f6f0df14c1806538b5ed40de110fd89a2c4d
Soobin Kwon <soobkwon@uw.edu>
2017-01-24 14:26:32 -0800

    Add Exercise 1 Module 10

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -1,35 +1,36 @@
 # Exercise 1: Data Frame Practice
 
 # Install devtools package: allows installations from GitHub
-install.packages('devtools')
+#install.packages('devtools')
 
 # Install "fueleconomy" package from GitHub
-devtools::install_github("hadley/fueleconomy")
+#devtools::install_github("hadley/fueleconomy")
 
 # Require/library the "fueleconomy" package
-library(fueleconomy)
+#library(fueleconomy)
 
 # You should now have access to the `vehicles` data.frame
 
 
 # Select the different manufacturers (makes) of the cars in this data set.
-
+manufacturers <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set.
+unique.car.manufacturers <- length(unique(manufacturers))
 
 
 # Filter the data set for vehicles manufactured in 1997
-
+cars.1997 <- manufacturers[vehicles$year == 1997]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function similar to how you would use the `max()` function.
 # See also: https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+hwy.gas.1997 <- vehicles[order(vehicles$hwy)]
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average gas milage between
 # city and highway for each car
-
+vehicles$average <- mean(vehicles$city + vehicles$hwy / 2)
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
 # Save this new data frame in a variable.
